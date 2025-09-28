afa06dcbd6f50903025363825876db773d8db49e
Akinori <71618777+S-Akinori@users.noreply.github.com>
2025-09-29 02:06:22 +0900

    Update BASE URL in app-config.js

--- config/app-config.js
+++ config/app-config.js
@@ -3,7 +3,7 @@
 //   - NG: CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET などサーバ秘密
 
 window.APP_CONFIG = Object.freeze({
-  BASE: 'https://script.google.com/macros/s/AKfycbwES9l2DfnuzR_d8aXBTsNO_AkKnZfmlI-ANiBoUWEemYebM_ykzdbV7mPDyr1c36WC/exec', // GAS Webアプリの/exec
+  BASE: 'https://script.google.com/macros/s/AKfycbz5yMUGWZFa9bawN381W9JA1VlYIBTeJKYtia8PhscH-n6-QCjFjvQNAS8TWpdbuKTw/exec', // GAS Webアプリの/exec
   LIFF_ID: '2008192477-4pJ07kDE',                                               // LINE Developers で発行
   // 環境を分けるなら任意で:
   ENV: 'prod', // 'stg' など
