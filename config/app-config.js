beecc4657b224fab0a2926fd6a0bc96e81dd5a6a
Akinori <71618777+S-Akinori@users.noreply.github.com>
2025-09-29 02:44:56 +0900

    Update app-config.js

--- config/app-config.js
+++ config/app-config.js
@@ -3,7 +3,7 @@
 //   - NG: CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET などサーバ秘密
 
 window.APP_CONFIG = Object.freeze({
-  BASE: 'https://script.google.com/macros/s/AKfycbyeYHUtQZvUmdmd85k-SWa5mBHIK5_ftMEt0_PuzpyabkKtgCaWx4eDMR446p0Kko5D/exec', // GAS Webアプリの/exec
+  BASE: 'https://script.google.com/macros/s/AKfycbxKXJTjnBc3u6uy8dvAfMifwNWOkMlRnadlEjE3ixcl7W5Gjx-on4NrA8LDN4LeUa6Z/exec', // GAS Webアプリの/exec
   LIFF_ID: '2008192477-4pJ07kDE',                                               // LINE Developers で発行
   // 環境を分けるなら任意で:
   ENV: 'prod', // 'stg' など
