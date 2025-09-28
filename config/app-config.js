f52d31207a4e2c29fac64ba0396bab421432b8c0
Akinori <71618777+S-Akinori@users.noreply.github.com>
2025-09-29 01:51:29 +0900

    Create app-config.js with application settings
    Add application configuration settings for the app.

---
+++ config/app-config.js
@@ -0,0 +1,10 @@
+// ※ 公開されるので「秘密トークン」は絶対に入れないでください
+//   - OK: LIFF_ID, GASの/exec URL
+//   - NG: CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET などサーバ秘密
+
+window.APP_CONFIG = Object.freeze({
+  BASE: 'https://script.google.com/macros/s/AKfycbwES9l2DfnuzR_d8aXBTsNO_AkKnZfmlI-ANiBoUWEemYebM_ykzdbV7mPDyr1c36WC/exec', // GAS Webアプリの/exec
+  LIFF_ID: '2008192477-4pJ07kDE',                                               // LINE Developers で発行
+  // 環境を分けるなら任意で:
+  ENV: 'prod', // 'stg' など
+});
